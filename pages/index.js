209362c7b3cc0d04840b5d0a9d7ef58d7cff3203
Akib Ahmed <akibahmed@iut-dhaka.edu>
2023-07-14 09:19:40 +0200

    Get started with api

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import styles from '@/styles/Home.module.css'
-
-export default function Home() {
+function HomePage() {
   return (
     <div>
-      <h1>Hello Next World!</h1>
+      <h1>The home page</h1>
     </div>
-  )
+  );
 }
+
+export default HomePage;
