8a942a4396fe3189e1da63a07d3ffd4e25f4df4f
Akib Ahmed <akibahmed@iut-dhaka.edu>
2023-07-14 11:34:50 +0200

    Dynamic API

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,17 +1,36 @@
+import { Fragment, useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
+
   return (
-    <ul>
-      {props.feedbackItems.map((item) => (
-        <li key={item.id}>{item.text}</li>
-      ))}
-    </ul>
+    <Fragment>
+        {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}
+            <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
 export async function getStaticProps() {
-    // haven't send an HTTP request because we are fetching api from the same project directory
+  // haven't send an HTTP request because we are fetching api from the same project directory
   const filePath = buildFeedbackPath();
   const data = extractFeedback(filePath);
   return {
